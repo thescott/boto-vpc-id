bb300ef522d253ba3fcdc75972bd9e7e17233324
Scott Mabe <Scott.Mabe@me.com>
2016-07-22 21:46:04 -0400

    reverting back to normal

--- getID/vpc.py
+++ getID/vpc.py
@@ -4,6 +4,6 @@ ec2 = boto3.resource ('ec2', region_name='us-east-1')
 
 filters = [{'Name':'tag:Name', 'Values':['Default']}]
 Default = list(ec2.vpcs.filter(Filters=filters))[0]
-Default.vpc_id 
+Default.vpc_id
 
 print (Default.vpc_id)
